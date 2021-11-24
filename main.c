585f82bf74b4807cadb122eac6a5558766069b7c
anAwesomeWave <timofey.zubov.05@gmail.com>
2021-11-24 18:03:24 +0300

    FINALLY write 'read_line' function that basically read line of random length

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <malloc.h>
 
-char* read_line() {
+char* read_line(int* len) { /* чтобы мы вне функции знали, какая длина у строки */
+    int total_size = 1; /* will double size of array if *len >= size - 1(because of '/0' symbol)*/
+    char* str = (char*)(malloc(sizeof(char))); /* FREE FREE FREE memory*/
+    /*if(str == NULL) ... */
+    printf("> ");
+    char c = (char)getchar();
+    while(c != '\n') { /* we don't need '\n' symbol itself */
+        str[(*len) ++] = c; /*БЛЯТЬ НЕ *(len) ++  А (*len) ++ это же приоритетность*/
+        if(*len == total_size) {
+            total_size *= 2;
+            str = (char *) realloc(str, total_size * sizeof(char)); /* FREE FREE FREE memory*/
+        }
+        c = (char)getchar();
+    }
+    str[*len] = '\0'; /* mb resize needed??*/
+    return str; /* FREE FREE FREE memory*/
 }
 
 
@@ -11,19 +27,17 @@ void run_loop() {
     int status = 0; /* DEBUG */
 
     do {
-        printf("> ");
-        line = read_line();
+        int len = 0;
+        line = read_line(&len);
+        printf("%s", line);
         /*args = (split_line(line));
         status = (execute(args))*/
-
+        free(line);
     }while(status);
 }
 
-int main(int argc, char** argv) {
-    printf("Have %d arguments\n", argc);
-    for(int i = 0; i < argc; i ++) {
-        printf("%s\n", *(argv + i));
-    }
-    run_loop()
+int main(int argc, char** argv) { /* argv - array of strings.
+                    lines are separated by spaces */
+    run_loop();
     return 0;
 }

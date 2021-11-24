73bac44ae19396c126b0f07a2f59f95c0cefcdf2
anAwesomeWave <timofey.zubov.05@gmail.com>
2021-11-24 16:51:11 +0300

    Wrote down main logic

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 
+char* read_line() {
+}
+
+
+
+void run_loop() {
+    char *line;
+    char **args;
+    int status = 0; /* DEBUG */
+
+    do {
+        printf("> ");
+        line = read_line();
+        /*args = (split_line(line));
+        status = (execute(args))*/
+
+    }while(status);
+}
+
 int main(int argc, char** argv) {
     printf("Have %d arguments\n", argc);
     for(int i = 0; i < argc; i ++) {
         printf("%s\n", *(argv + i));
     }
+    run_loop()
     return 0;
 }

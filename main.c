72e561b466be87739146b25a1031a547c6d95994
anAwesomeWave <timofey.zubov.05@gmail.com>
2021-11-25 21:33:29 +0300

    Wrote 'split_line' function that split line by spaces(memory leak may occur)

--- main.c
+++ main.c
@@ -2,16 +2,28 @@
 #include <string.h>
 #include <malloc.h>
 
+
+/*void free_mem(char** a, char*** b) {
+    free(*a);
+    char** ptr = *b;
+    while(*ptr != NULL) {
+        free(*ptr);
+        ptr ++;
+    }
+    //free(*ptr);
+    free(*b);
+}*/
+
 char* allocation_error() {
     printf("allocation error occurred");
     return NULL;
 }
 
-char* read_line(int* len) { /* чтобы мы вне функции знали, какая длина у строки */
+void* read_line(int* len) { /* чтобы мы вне функции знали, какая длина у строки */
     int total_size = 1; /* will double size of array if *len >= size - 1(because of '/0' symbol)*/
     char* str = (char*)(malloc(sizeof(char))); /* FREE FREE FREE memory*/
     if(str == NULL) {
-        return allocation_error();
+        return (char*)allocation_error();
     }
     printf("> ");
     char c = (char)getchar();
@@ -21,7 +33,7 @@ char* read_line(int* len) { /* чтобы мы вне функции знали,
             total_size *= 2;
             str = (char *) realloc(str, total_size * sizeof(char)); /* FREE FREE FREE memory*/
             if(str == NULL) {
-                return allocation_error();
+                return (char*)allocation_error();
             }
         }
         c = (char)getchar();
@@ -33,23 +45,25 @@ char* read_line(int* len) { /* чтобы мы вне функции знали,
 char** split_line(char* line) {
     /*will use strtok -> no need in length of line */
     /* для strtok нужны разделители, не содержащиеся в исходной строке*/
-    char delim[] = " ";
-    char** args = (char **) malloc(sizeof(char*)); /*one line */
+    char delim[] = " \t\r\n\v\f";
+    char** args = (char **) malloc(sizeof(char*)); /*one line here we care only about memory for pointer
+ *  this pointer can point at array of any size but we have to allocate memory for it
+ *  it's similar to 2D vector
+ * */
     if(!args)
         return (char **) allocation_error();
     int argcnt = 0; /* number of lines in args */
     int sz = 1;
-    char* newArg = strtok(line, " \t\r\n\v\f");
+    char* newArg = strtok(line, delim); // TODO: rewrite without strtok
     while(newArg != NULL) {
-        //args[argcnt] = (char*) malloc(sizeof(char) * strlen(newArg)); // allocate the necessary amount of bytes for
-        // every char of new command
         args[argcnt ++] = newArg;
-        printf("%s", args[argcnt-1]);
         if(argcnt == sz) {
             sz *= 2;
             args = (char**) realloc(args, sizeof(char*) * sz);
+            if(args == NULL)
+                return (char**) allocation_error();
         }
-        newArg = strtok(NULL, " \t\r\n\v\f"); // subcall
+        newArg = strtok(NULL, delim); // subcall
     }
     args[argcnt] = NULL; // end of sequence
     return args;
@@ -64,20 +78,25 @@ void run_loop() {
     do {
         int len = 0;
         line = read_line(&len);
-        if(line == NULL) {
+
+        if(line == NULL)
             return;
-        }
         args = split_line(line);
-        char* ptr = *args;
-        while(ptr != NULL) {
-            //printf("%s", ptr);
+        if(args == NULL)
+            return;
+
+        char** ptr = args; // BUG WAS HERE
+        // char* ptr = *args;
+        while(*ptr != NULL) {
+            printf("%s\n", *ptr);
             ptr ++;
         }
 
-        /*args = (split_line(line));
-        status = (execute(args))*/
+        //free_mem(&line, &args);
         free(line);
-    }while(status);
+        free(args); // memory leak may occur. check later with valgrind
+        // TODO: check for memory leaking.
+    }while(0);
 }
 
 int main(int argc, char** argv) { /* argv - array of strings.

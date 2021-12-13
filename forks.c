2c1b826a7e4a91b21e49bef136807e3246546ca4
anAwesomeWave <timofey.zubov.05@gmail.com>
2021-12-14 00:08:18 +0300

    Dealing with init process.

--- forks.c
+++ forks.c
@@ -3,6 +3,7 @@
 #include<unistd.h> // for fork, exec
 #include<sys/types.h>  // for clock_t (some basic derived types)
 #include<sys/wait.h> // for wait
+#include<stdlib.h> // for exit()
 
 int main() {
     printf("hello\n");
@@ -13,10 +14,15 @@ int main() {
     }
     if(process == 0) {
         printf("This is child process.\nMy PID is %d\n",  getpid()); // getpid return pid_t (int)
-        printf("PID of parent - %d", getppid());
+        printf("PID of parent - %d\n", getppid());
+        sleep(3);
+        printf("PPID of orphan - %d\n(init process - parent of all processes. "
+               "first process to start when a computer boots up and runs until the system shuts down.)", getppid());
+        // after parent's process has finished running
     }else{
+        sleep(1);
         printf("This is parent. My PID is %d\n", getpid());
         printf("PID OF child process is %d\n", process);
-
+        exit(0);
     }
 }

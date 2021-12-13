0ed9606541eab931e859721718c8b42504dbfe4c
anAwesomeWave <timofey.zubov.05@gmail.com>
2021-12-13 23:41:31 +0300

    Forking 1

--- forks.c
+++ forks.c
@@ -6,4 +6,10 @@
 
 int main() {
     printf("hello\n");
+    int process = fork();
+    if(process < 0) {
+        printf("An error occurred while creating a child process.");
+        return -1;
+    }
+    printf("123\n"); // call twice
 }
